c14f7948ef73b1606b4d80cc5c466173e3f79252
yuntao-github <55419339+yuntao-github@users.noreply.github.com>
2024-09-17 18:35:25 +0800

    Add files via upload

---
+++ A2E_app_GUI/run2generate_protected_code.m
@@ -0,0 +1,17 @@
+clear
+script2protect = {"plot_output_figures4GUI.m"};
+% 
+% "binning.m";
+%     "closure_depth6_prior_model1227.m";
+%     "closure_temps.m";
+%     "closure_temps_linear.m";
+%     "inversion_clc2.m";
+%     "isotherms_clc0909.m";
+%     "linexline.m";
+%     "master4GUI.m";
+%     "temp_mean.m";
+%     "tridag.m";
+% for i=1:size(script2protect,1)
+%     pcode(script2protect{i,1});
+%     % delete(script2protect{i,1})
+% end
